f91098e4eed4efd382e0733aa5ebaeb4c65cc5b4
Ugis-B <54326661+Ugis-B@users.noreply.github.com>
2020-01-10 17:02:22 +0200

    add css

--- main.py
+++ main.py
@@ -1,14 +1,36 @@
-from flask import Flask, render_template
+from flask import Flask, json, jsonify, render_template, request
 
 app = Flask('app')
 
+
 @app.route('/')
 def index_lapa():
-  return render_template('index.html')
+  return render_template('chats.html')
+
 
 @app.route('/health')
-def health_page():
+def health_check():
   return "OK"
+
+
+@app.route('/chats/lasi')
+def ielasit_chatu():
+  chata_rindas = []
+  with open("chats.txt", "r") as f:
+    for rinda in f:
+      chata_rindas.append(rinda)
+  return jsonify({"chats": chata_rindas})
+
+
+@app.route('/chats/suuti', methods=['POST'])
+def suutiit_zinju():
+  dati = request.json
+  
+  with open("chats.txt", "a", newline="") as f:
+    f.write(dati["chats"] + "\n")
+
+  return ielasit_chatu()
+  
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
--- static/chats.js
+++ static/chats.js
@@ -1,1 +1,98 @@
-console.log("Sveiciens no konsoles");
+const ATJAUNOT = 50000;
+const VERSIJA = "0.4"
+
+/*
+Ielādē tērzēšanas datus no servera
+Uzstāda laiku pēc kāda atkārtoti izsaukt šo pašu funkciju
+*/
+async function lasiChatu() {
+    const atbilde = await fetch('/chats/lasi');
+    const datuObjekts = await atbilde.json();
+    raadiChataRindas(datuObjekts);
+    await new Promise(resolve => setTimeout(resolve, ATJAUNOT));
+    await lasiChatu();
+}
+
+
+function raadiChatuVienkarsi(dati) {
+    var jaunaRinda = "<br/>";
+    var chats = "";
+    var chataDiv = document.getElementById("chats");
+    for (var rinda of dati["chats"]) {
+        chats = chats + rinda + jaunaRinda;
+    }
+    chataDiv.innerHTML = chats;
+}
+
+
+function raadiChataRindas(dati) {
+    const chatUL = document.getElementById("chats");
+    // novaacam ieprieksheejo saturu
+    while (chatUL.firstChild) {
+        chatUL.firstChild.remove();
+    }
+    for (let rinda of dati["chats"]) {
+      chatLI = izveidoJaunuRindu(rinda);
+      chatUL.appendChild(chatLI);
+    }
+    // noskrolleejam uz leju pie peedeejaa chata texta
+    var chatScrollBox = chatUL.parentNode;
+    chatScrollBox.scrollTop = chatScrollBox.scrollHeight;
+  }
+  
+  
+  function izveidoJaunuRindu(zinja) { 
+    let newLI = document.createElement("li");
+    newLI.className = "left clearfix"
+    let newDiv = document.createElement("div"); 
+    newDiv.className = "chat-body clearfix"
+    let newContent = document.createTextNode(zinja); 
+    newLI.appendChild(newDiv); 
+    newDiv.appendChild(newContent); 
+    return newLI;
+  }
+
+
+/*
+Publicē tērzēšanas ziņas datus uz serveri
+*/
+async function suutiZinju() {
+    // Nolasa ievades lauka saturu
+    let zinjasElements = document.getElementById("zinja");
+    const zinja = zinjasElements.value;
+
+    // pārbaudām vai ir vispār kaut kas ierakstīts
+    if (zinja.length > 0) {
+
+        // izdzēš ievades lauku
+        zinjasElements.value = "";
+
+        const atbilde = await fetch('/chats/suuti', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ "chats": zinja })
+        });
+        const datuObjekts = await atbilde.json();
+        // parāda jauno chata saturu
+        raadiChataRindas(datuObjekts);
+    } else {
+        console.log("Tukšu ziņu nesūtām uz serveri")
+    }
+
+}
+
+
+// Ērtības funkcionalitāte
+var versijasLauks = document.getElementById("versija");
+versijasLauks.innerHTML = "JS versija: " + VERSIJA;
+// Atrod ievades lauku
+var ievadesLauks = document.getElementById("zinja");
+// Gaida signālu no klaviatūras, ka ir nospiests Enter taustiņš
+ievadesLauks.addEventListener("keyup", function(event) {
+  // Numur 13 ir "Enter" taustiņš
+  if (event.keyCode === 13) {
+    suutiZinju();
+  }
+});

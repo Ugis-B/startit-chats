e27af87830cdccd4493472d6ecf9ccb392d25920
Ugis-B <54326661+Ugis-B@users.noreply.github.com>
2020-01-10 11:50:15 +0200

    pievienots javascrip

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask
 
 
 app = Flask('app')
-
+app.debug=True
 
 @app.route('/')
 def index_page():
@@ -11,4 +11,4 @@ def index_page():
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
-    app.run(threaded=True, port=5000)
+    app.run(threaded=True, port=5000, debug=True)
